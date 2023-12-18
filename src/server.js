73c1d09634be60358df95f3cc54d239652ff63ea
Julie <j.prunaret@gmail.com>
2023-12-18 13:00:19 +0100

    build - install Cors| npm install cors + config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+// Using CORS to resolve autorisation's issues
+app.use(cors());
 
 const links = require("../datas/links.json");
 const nodes = require("../datas/nodes.json");
